99a75329b77a61525b77d75bb658c7e963af1442
Flash-KIM142 <topqr123q@gmail.com>
2023-07-31 21:12:44 +0900

    feat: add UserServiceTest

---
+++ src/test/java/com/todolist/service/UserServiceTest.java
@@ -0,0 +1,21 @@
+package com.todolist.service;
+
+import com.todolist.repository.UserRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+
+}
